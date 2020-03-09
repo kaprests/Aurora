88aa303c88531b99d0f1564635823d947d7ce4df
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-03-09 07:49:31 +0100

    Adjust params, adds plot of yz-plane

--- aurora_sim.py
+++ aurora_sim.py
@@ -3,32 +3,44 @@ from matplotlib import pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.constants import mu_0, elementary_charge, proton_mass
 
-
 ####################
 ### Define stuff ###
 ####################
 
-
-### Dipole ###
-dipole_magnitude = 3
-tilt = 11 # degrees, arb. chosen
+### Physical parameters ###
+''' Dipole '''
+dipole_magnitude = 1e5 # No physical reasoning behind this number
+tilt = 11 # degrees
 tilt = np.radians(tilt) # angle between z-axis, tiltend about y-axis
 dp_x = np.sin(tilt)*dipole_magnitude
 dp_y = 0
 dp_z = np.cos(tilt)*dipole_magnitude
 dipole = np.array([dp_x, dp_y, dp_z])
 
+
+R = 6.37e6 # meter, earth's radius
+R_sim = 10 # Units of space, earth's radius in simulation
+sim_space_unit = R/R_sim # Number of meters per space unit
+V0 = 450*1000 # Thypical solar wind speeds ~ 10km/h, earth radii per second (m/s)
+V0_sim = V0/sim_space_unit # Units of space per second
+print("Units of space per second: ", V0_sim)
+
 ### Space ###
-xyz_lim = 10
 N = 100
-x = np.linspace(-xyz_lim, xyz_lim, N)
-y = np.linspace(-xyz_lim, xyz_lim, N)
-z = np.linspace(-xyz_lim, xyz_lim, N)
+x = np.linspace(-80, 20, N)
+y = np.linspace(-80, 20, N)
+z = np.linspace(-40, 60, N)
 
 ### simulation params ###
-dt = 0.1
-SIM_TIME = 100
-INITIAL = [-10, 0, -8, 0.55, 0, 0] # [xo, y0, z0, dx0/dt, dy0/dt, dz0/dt]
+dt = 1 # Seconds??
+SIM_TIME = 500 # dts
+INITIALS = [
+        [-20, -20, 40, V0_sim, V0_sim, 0],
+        [-20, -20, 30, V0_sim, V0_sim, 0],
+        [-20, -20, 20, V0_sim, V0_sim, 0],
+        [-20, -20, 10, V0_sim, V0_sim, 0],
+        [-20, -20, -10, V0_sim, V0_sim, 0],
+        ] # [xo, y0, z0, dx0/dt, dy0/dt, dz0/dt]
 
 
 ### Functions ###
@@ -68,26 +80,50 @@ def solve_equation_of_motion(ddt_vel_accl, SIM_TIME, INITIAL, dt):
 ######################
 ### Simulate stuff ###
 ######################
-p, v = solve_equation_of_motion(ddt_vel_accl, SIM_TIME, INITIAL, dt)
-print(p)
-print("shape: ", p.shape[1])
-print(SIM_TIME/dt)
-print("len: ", len(p))
-print("elementary charge: ", elementary_charge)
+paths = []
+velocities = []
 
+for initial in INITIALS:
+    path, velocity = solve_equation_of_motion(ddt_vel_accl, SIM_TIME, initial, dt)
+    paths.append(path)
+    velocities.append(velocity)
+
+#print("Velocity: ", V0)
+#print("Simulation time: ", SIM_TIME)
+#print("distance units moved (if straight line): ", np.sqrt((path[0][-1]-path[0][0])**2 + (path[1][-1]-path[1][0])**2 + (path[2][-1]-path[2][0])**2))
 
 ##################
 ### Plot stuff ###
 ##################
 
-### B-field ###
+### B-field xz-plane ###
 xx, zz = np.meshgrid(x, z)
 B_xx, B_yy, B_zz = B_field(xx, np.zeros_like(xx), zz)
 plt.streamplot(xx, zz, B_xx, B_zz)
-earth = plt.Circle((0, 0), 1, color="blue")
+earth = plt.Circle((0, 0), R_sim, color="blue")
 plt.gca().add_artist(earth)
 
-plt.plot(p[0], p[2])
+for i in range(len(paths)):
+    path = paths[i]
+    x0 = (INITIALS[i][0], INITIALS[i][1], INITIALS[i][2])
+    v0 = (INITIALS[i][3], INITIALS[i][4], INITIALS[i][5])
+    plt.plot(path[0], path[2], label="Start pos: " + str(x0) + "v0: " + str(v0))
+plt.legend()
+plt.show()
+
+
+yy, zz = np.meshgrid(y, z)
+B_xx, B_yy, B_zz = B_field(np.zeros_like(yy), yy, zz)
+plt.streamplot(yy, zz, B_yy, B_zz)
+earth = plt.Circle((0, 0), R_sim, color="blue")
+plt.gca().add_artist(earth)
+
+for i in range(len(paths)):
+    path = paths[i]
+    x0 = (INITIALS[i][0], INITIALS[i][1], INITIALS[i][2])
+    v0 = (INITIALS[i][3], INITIALS[i][4], INITIALS[i][5])
+    plt.plot(path[1], path[2], label="Start pos: " + str(x0) + "v0: " + str(v0))
+plt.legend()
 plt.show()
 
 
